4a55330d14f573afaf386ec89392725a786ea8c7
Brown-scripts <nikkibrowm297@gmail.com>
2025-01-05 00:51:50 +0000

    fixed credential leak

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -4,8 +4,8 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   auth: {
-    user: 'nikkibrowm297@gmail.com',
-    pass: 'jbzjqieokndmljwp',
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
